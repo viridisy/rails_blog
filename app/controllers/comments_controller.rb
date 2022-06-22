b9f3190a7d3408bcf54847156e066eee1e2c18b3
viridisy <viridisy@yahoo.co.jp>
2022-06-22 23:27:56 +0900

    secound

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,7 +11,7 @@ class CommentsController < ApplicationController
         @article = Article.find(params[:article_id])
         @comment = @article.comments.find(params[:id])
         @comment.destroy
-        redirect_to article_path(@article), status: 303
+        redirect_to article_path(@article),  status: 303
       end    
     
     private
