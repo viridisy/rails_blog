a88dbeee252f374139803159a9425d257f1193fb
viridisy <viridisy@yahoo.co.jp>
2022-06-29 22:43:11 +0900

    second

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,9 @@
 class Article < ApplicationRecord
-    has_many :comments, dependent: :destroy
+include Visible
+  
+  has_many :comments, dependent: :destroy
 
-    validates :title, presence: true
-    validates :body, presence: true, length: { minimum: 10 }
-  end
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+end
   
